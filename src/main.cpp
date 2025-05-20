4593163e1e9ff3c3c8805786156e90cf7bb98e9f
xitman327 <mcxitos327@gmail.com>
2025-05-20 22:32:06 +0300

    firebase changeover

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,53 @@
 #define DEMO_MODE 1
 #define DEBUG_SERIAL Serial
-
+#include "user.h"
 #include "WiFi.h"
-#include "WebServer.h"
+#include <WiFiClientSecure.h>
+#include <WebServer.h>
 #include "AutoConnect.h"
 #include "ESPTelnet.h"
 
 ESPTelnet telnet;
 
 WebServer Server;
-WebServer websoc(3232);
-AutoConnect  Portal(Server);
-AutoConnectConfig Config;
+AutoConnect       Portal(Server);
+AutoConnectConfig Config;       // Enable autoReconnect supported on v0.9.4
 
-AutoConnectAux Data("/data", "Data");
-
+#include <FirebaseClient.h>
 
 
-const char PAGE_MAIN[] PROGMEM = R"=====(
-  <html>
-    <style>
-     .fanrpmslider {
-      width: 30%;
-      height: 55px;
-      outline: none;
-    }
-     .bodytext {
-      font-family: "Verdana", "Arial", sans-serif;
-      font-size: 24px;
-      text-align: left;
-      font-weight: light;
-      border-radius: 5px;
-      display:inline;
-    }
-    </style>
-    <body>
-      <header>
-      
-      </header>
-      <main>
-          <div class="bodytext">Fan Speed Control (RPM: <span id="fanrpm"></span>)</div>
-    <br>
-          <input type="" class="fanrpmslider" min="0" max="255" value = "0" width = "0%" oninput="UpdateSlider(this.value)"/>
-      </main>
-      <footer>
-      
-      </footer>
-    </body>
-    <script>
-    
-    </script>
-  </html>
-  
-  )=====";
+/*
+
+follow guide 
+https://randomnerdtutorials.com/esp32-firebase-realtime-database/
+
+create a user.h file inside include folder and populate it with the following
+
+#define Web_API_KEY "REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
+#define DATABASE_URL "REPLACE_WITH_YOUR_FIREBASE_DATABASE_URL"
+#define USER_EMAIL "REPLACE_WITH_FIREBASE_PROJECT_EMAIL_USER"
+#define USER_PASS "REPLACE_WITH_FIREBASE_PROJECT_USER_PASS"
+
+*/
+
+void processData(AsyncResult &aResult);
+
+UserAuth user_auth(Web_API_KEY, USER_EMAIL, USER_PASS);
+
+FirebaseApp app;
+WiFiClientSecure ssl_client;
+using AsyncClient = AsyncClientClass;
+AsyncClient aClient(ssl_client);
+RealtimeDatabase Database;
+
+// Timer variables for sending data every 10 seconds
+unsigned long lastSendTime = 0;
+const unsigned long sendInterval = 10000; // 10 seconds in milliseconds
 
 
-#include <ESP_Google_Sheet_Client.h>
-
-// Google Project ID
-#define PROJECT_ID "lexical-pattern-446023-p9"
-
-// Service Account's client email
-#define CLIENT_EMAIL "esp32-car-logger@lexical-pattern-446023-p9.iam.gserviceaccount.com"
-
-// Service Account's private key
-const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC3lsWNFAY9/mw4\n3oqEljiZ+JGMgEqmTPr0crfJBkCy8cOOxdHnyqx/ke5u22Ex3UhTJr60tti91C2E\nuPGah/YsNMNF29c+cUwuOS8c0Zzoyb7C16QrnrGg60GbLNz3EUIF38Osv+RFRwCi\nfSl1G/rXJnyrQRjwz2fpXtp2T8qQGC3+CJZ7/FBFBRgFel8GcV6f72x604Y+dzhl\nnhhJDNRyMaJCvRXgD8Rhcmdc8gphlKmccMQIMrkmYDxEXOq6o2AFlCT2UpXD70+T\nyJfzG4UBCdM73DxZggI9ILCaQBrP8cHb1dsyLauX02GMU2/LU3QHF8C/hhzFDiui\ns5givLmDAgMBAAECggEADXxmU52HyIWy0R9L1pSAYOhuV1Sp5yXFfi/FH1+1tfoQ\nzvaUxkL66+i+NpIn8B/yAFBazESYOaLnRcDI1/yqe9x+XFiGAGDAjgQGWXFuZiKX\nwDv9+R77AQ1MWvKZ4rj3psh6rmsZgMXynn0KOWo47BVi2A5RSjM6I12j4kgB4xa7\nwM4gsas/NHhcMIRC1+b1uF4EEV75xH7/jtWYYJto8KWeTA2d9aAm9yVZSU6M4/yq\nw5lwtegb8NxJnZeTSDvV09hU2OmPCHFI8pEYHxdC/azjGpcqz/qsuTBhdTilu0C6\n8wZFBwKfVdyeHaqZR6hU96JZXUlNSHdNHSeeZyQrhQKBgQDfZL2XQ8gUizd7kcKb\nIdLAlRgBcv7xTjEaYcBOPeWtbEf/lQnrvBaBCRsTL4Mf4qZ74ofVsAU2CoUswVe9\n97I7xILf3Vihu6+rmLp06W7ueL8aZzg1YjcRvpApRWIDkCQGzzl4JOfNs8H+j21n\n+BFmyQx+lacKa0bEdAHEPeB5dwKBgQDSYrRyHjHvigDEZyTtEckiXP7MdKvZpNsL\nMNGgjiNAFXj1Keggq8xHpTesvAaL/ZNkpvEPNOws4omBy1R3sDVxhMv/WAEXNVWJ\nj1XaWoIs7WwRBBhYy/IfxD8m712uZATPhr7TIf6wg7lBwVjpgooHN65ysE0iXn+O\nuKzMqngDVQKBgG5MKSfUeadbDWvfjfxWeN5JPu8IPkQaXTgvZ2m6OfqkafARQDK6\nTUosmsegP9ewao7kTDj/jbMfAp4UYCQVdlT4M74gZbCFILlS0U0ELJdl2sVIjYIe\nGh6Svk8CSOCFfKQ4EKH3ZtQzmub7HgOlgcIEJj0h2rXUPb6loHGqM3kFAoGBAMc7\nwCRd4e41Y1qbTwXOlQocxRL73g5jJyhm1+3TDSDP9Nc/E1t1NiQXGy1SdmS2A5IK\n4jgDZzFuasfmtRwPW90f5EvwnD/NOy7k9Wmt0p8XTAKlPSVCJg3dO+z0O2Q9xax9\nr1KkW9vvMk5J+phQDcSd/28O9Ez67pNK4iGDwdltAoGBALSmyAehM7oR+RdzXd/C\nHx4R9OqxVxuSo0G6vn2sL3n3u6UtIcXpwj28SRtrDpBZj1NLBJTMakGmNx4nbpwo\nWeokcT2o3beQ1QwgI4V5WyFI6VInZNKZTIGhx35v5go7hBAdcD8ZsMBHtekG3sET\nAJGf3FjRmWDQrjyQw71yugwA\n-----END PRIVATE KEY-----\n";
-
-// The ID of the spreadsheet where you'll publish the data
-const char spreadsheetId[] = "1Igi_Eu8ytkM2I7W8L3MctP7x26rzCqba0UuEbO5jsLQ";
+int intValue = 0;
+float floatValue = 0.01;
+String stringValue = "";
 
 
 #include "ELMduino.h"
@@ -87,7 +67,7 @@ typedef enum { ENG_RPM,
                SPEED ,
                MAF,
                FUEL_LEVEL
-               } obd_pid_states;
+                          } obd_pid_states;
 obd_pid_states obd_state = ENG_RPM;
 
 void init_elm(){
@@ -104,52 +84,6 @@ void init_elm(){
   
 }
 
-void tokenStatusCallback(TokenInfo info){
-    if (info.status == token_status_error){
-        GSheet.printf("Token info: type = %s, status = %s\n", GSheet.getTokenType(info).c_str(), GSheet.getTokenStatus(info).c_str());
-        GSheet.printf("Token error: %s\n", GSheet.getTokenError(info).c_str());
-    }
-    else{
-        GSheet.printf("Token info: type = %s, status = %s\n", GSheet.getTokenType(info).c_str(), GSheet.getTokenStatus(info).c_str());
-    }
-}
-
-void init_Gsheets(){
-  // Set the callback for Google API access token generation status (for debug only)
-    GSheet.setTokenCallback(tokenStatusCallback);
-
-    // Set the seconds to refresh the auth token before expire (60 to 3540, default is 300 seconds)
-    GSheet.setPrerefreshSeconds(10 * 60);
-
-    // Begin the access token generation for Google API authentication
-    GSheet.begin(CLIENT_EMAIL, PROJECT_ID, PRIVATE_KEY);
-}
-
-void append_gsheets(){
-
-  init_Gsheets();
-
-  FirebaseJson response;
-  FirebaseJson valueRange;
-
-  valueRange.add("majorDimension", "COLUMNS");
-  valueRange.set("values/[0]/[0]", fuel_level);
-  valueRange.set("values/[1]/[0]", rpmn);
-  valueRange.set("values/[2]/[0]", kmph);
-  valueRange.set("values/[3]/[0]", lpkm);
-
-  // For Google Sheet API ref doc, go to https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append
-  // Append values to the spreadsheet
-  bool success = GSheet.values.append(&response /* returned response */, spreadsheetId /* spreadsheet Id to append */, "Sheet1!A1" /* range to append */, &valueRange /* data range to append */);
-  if (success){
-      response.toString(Serial, true);
-      valueRange.clear();
-  }
-  else{
-      DEBUG_SERIAL.println(GSheet.errorReason());
-  }
-}
-
 void telnet_onconnect(String ip){
 
   DEBUG_SERIAL.println("hello!");
@@ -159,33 +93,53 @@ void telnet_onconnect(String ip){
 
 }
 
+
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now;
+}
+
 void setup()
 {
 
   Serial.begin(115200);
   ELM_PORT.begin(38400, SERIAL_8N1, 17, 18, false, 2000);
-  // telnet.onConnect(telnet_onconnect);
-  // DEBUG_SERIAL.println("Starting");
-  // Server.on("/", [](){Server.send(200, "text/html", PAGE_MAIN);} );
-  // Server.on("/data", [](){DEBUG_SERIAL.println("starting gshets!"); append_gsheets(); Server.send(200, "text", "ok");} );
+  telnet.onConnect(telnet_onconnect);
+  DEBUG_SERIAL.println("Starting");
 
-  // Config.autoReset = false;
-  // Config.autoReconnect = true;
-  // Config.reconnectInterval = 6;
-  // Config.retainPortal = true;
-  // Config.ota = AC_OTA_BUILTIN;
-  // Portal.config(Config);
-  // Portal.join(Data);
-  // Portal.begin();
+  Config.autoReset = false;
+  Config.autoReconnect = true;
+  Config.reconnectInterval = 6;
+  Config.retainPortal = true;
+  Config.ota = AC_OTA_BUILTIN;
+  Portal.config(Config);
+  Portal.begin();
 
-  WiFi.begin("Wokwi-GUEST", "", 6);
+  // WiFi.begin("Wokwi-GUEST", "", 6);
+
+  configTime(0, 0, ntpServer);
 
   if(!DEMO_MODE){
     init_elm();
   }
 
-  // while(!WiFi.isConnected() && millis() < 10000){}
-  // telnet.begin(23);
+  ssl_client.setInsecure();
+  // ssl_client.setConnectionTimeout(1000);
+  ssl_client.setHandshakeTimeout(5);
+  
+  // Initialize Firebase
+  initializeApp(aClient, app, getAuth(user_auth), processData, "🔐 authTask");
+  app.getApp<RealtimeDatabase>(Database);
+  Database.url(DATABASE_URL);
+
+  while(!WiFi.isConnected() && millis() < 10000){}
+  telnet.begin(23);
 
 }
 
@@ -199,7 +153,32 @@ float lts_trip;
 
 void loop()
 {
-  Portal.handleClient();
+  app.loop();
+
+  if(app.ready()){
+    unsigned long currentTime = millis();
+    if (currentTime - lastSendTime >= sendInterval){
+      lastSendTime = currentTime;
+      
+      // send a string
+      stringValue = "value_" + String(currentTime);
+      Database.set<String>(aClient, "/test/string", stringValue, processData, "RTDB_Send_String");
+      // send an int
+      Database.set<int>(aClient, "/test/int", intValue, processData, "RTDB_Send_Int");
+      intValue++; //increment intValue in every loop
+
+      // send a string
+      floatValue = 0.01 + random (0,100);
+      Database.set<float>(aClient, "/test/float", floatValue, processData, "RTDB_Send_Float");
+
+    }
+  }
+
+
+
+
+
+
   telnet.loop();
 
   if(millis() - tm_obdpull > obd_pull_time){
@@ -354,3 +333,32 @@ Diesel: 14.6
 
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+void processData(AsyncResult &aResult) {
+  if (!aResult.isResult())
+    return;
+
+  if (aResult.isEvent())
+    Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.eventLog().message().c_str(), aResult.eventLog().code());
+
+  if (aResult.isDebug())
+    Firebase.printf("Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
+
+  if (aResult.isError())
+    Firebase.printf("Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
+
+  if (aResult.available())
+    Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
+}
